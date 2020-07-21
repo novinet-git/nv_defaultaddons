6c2ee2c0143cfb01f8459f37a92691f191e176be
Daniel Steffen <daniel.steffen@novinet.de>
2020-07-21 17:53:03 +0200

    Icon Added

--- boot.php
+++ boot.php
@@ -1,1 +1,6 @@
 <?php $addon = rex_addon::get("nv_defaultaddons");
+
+
+if (file_exists($addon->getAssetsPath("css/style.css"))) {
+    rex_view::addCssFile($addon->getAssetsUrl("css/style.css"));
+}
